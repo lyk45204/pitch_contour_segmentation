5afc9b1ab1bb3bead64fcdf100043aa3ec40b72e
Yukun Li <yukun.li@qmul.ac.uk>
2024-02-21 15:39:20 +0800

    Delete enviroment directory

--- enviroment/QuantumSample/interpolaFn.m
+++
@@ -1,5 +0,0 @@
-function [interpolatedTime,interpolatedSpitch] = interpolaFn(originalTime,originalPitch,InsertTimes)
-    f0Fs = 1/(originalTime(2)-originalTime(1));
-    interpolationF = InsertTimes*f0Fs;  %interpolation frequency
-    interpolatedTime = [originalTime(1):1/interpolationF:originalTime(end)]';
-    interpolatedSpitch = spline(originalTime,originalPitch,interpolatedTime);
